eca4a9c5f80f2594453cbf2dd64e60ecdbf57322
Lim Jong Hyun <scoke0801@gmail.com>
2021-03-26 12:24:58 +0900

    padding, margin 추가,  onTouchEvent 추가

--- CustomControls/app/src/main/java/kr/ac/kpu/s2015182034/customcontrols/MainActivity.java
+++ CustomControls/app/src/main/java/kr/ac/kpu/s2015182034/customcontrols/MainActivity.java
@@ -3,15 +3,22 @@ package kr.ac.kpu.s2015182034.customcontrols;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        MyView mv = new MyView(this);
-        setContentView(mv);
+        //MyView mv = new MyView(this);
+        //setContentView(mv);
     }
+
+    // 방법 1
+    //@Override
+    //public boolean onTouchEvent(MotionEvent event) {
+    //   return super.onTouchEvent(event);
+    //}
 }
