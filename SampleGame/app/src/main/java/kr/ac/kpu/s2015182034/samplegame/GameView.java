a4807022c277d0df358fc1ce4cab04857c598622
Lim Jong Hyun <scoke0801@gmail.com>
2021-03-29 11:56:23 +0900

    게임 루프 정의 기초 - 루프는 아직 돌지않음

--- SampleGame/app/src/main/java/kr/ac/kpu/s2015182034/samplegame/GameView.java
+++ SampleGame/app/src/main/java/kr/ac/kpu/s2015182034/samplegame/GameView.java
@@ -6,22 +6,51 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
-    private final Bitmap bitmap;
+    // final 변수는 생성자에서 그 값이 결정되어야 한다.
+    private static final String TAG = GameView.class.getSimpleName();
+
+    private Bitmap bitmap;
+
+    private float x;
+    private float y;
 
     // 생성자 종류, 필요에 맞게 정의 필요
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        InitResources();
+
+        startUpdating();
+    }
+
+    private void startUpdating() {
+        doGameFrame();
+    }
+
+    private void doGameFrame() {
+        // update();   // 계산
+        x += 0.01;
+        y += 0.01;
+        // draw();     // 그리기
+        // invalidate()함수는 여러번 중첩 호출되어도 한번에 그림으로써 중첩 호출을 해결
+        invalidate();
+    }
+    private void InitResources() {
         Resources res = getResources();
         bitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+
+        x = 100;
+        y = 100;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawBitmap(bitmap,0,0, null);
+        canvas.drawBitmap(bitmap,x, y, null);
+        Log.d(TAG,  "Drawing at : " + x + " y : " + y);
     }
 }
