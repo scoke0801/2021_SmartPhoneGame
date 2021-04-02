8d70466404ff78c604fedfbdb72aa1733a1f529a
Lim Jong Hyun <scoke0801@gmail.com>
2021-04-02 11:42:11 +0900

    ArrayList를 이용하여 공 객체들을 관리[update, draw..]

--- SampleGame/app/src/main/java/kr/ac/kpu/s2015182034/samplegame/Ball.java
+++ SampleGame/app/src/main/java/kr/ac/kpu/s2015182034/samplegame/Ball.java
@@ -9,9 +9,11 @@ public class Ball {
     private float x, y;   // 위치
     private float dx, dy; // 속도
 
+    private static int sx, sy; // 크기
+
     private static Bitmap bitmap;
 
-    public Ball(float x, float y, int dx, int dy) {
+    public Ball(float x, float y, float dx, float dy) {
         this.x = x;
         this.y = y;
         this.dx = dx;
@@ -20,12 +22,24 @@ public class Ball {
         if(bitmap == null) {
             Resources res = GameView.view.getResources();
             bitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+            sx = bitmap.getWidth();
+            sy = bitmap.getHeight();
         }
     }
 
     public void update() {
         this.x += this.dx * GameView.frameTime;
         this.y += this.dy * GameView.frameTime;
+
+        int w = GameView.view.getWidth();
+        int h = GameView.view.getHeight();
+
+        if(x < 0 || x + sx > w){
+            dx *= -1;
+        }
+        if(y < 0 || y + sy > h){
+            dy *= -1;
+        }
     }
 
     public void draw(Canvas canvas) {
--- SampleGame/app/src/main/java/kr/ac/kpu/s2015182034/samplegame/GameView.java
+++ SampleGame/app/src/main/java/kr/ac/kpu/s2015182034/samplegame/GameView.java
@@ -1,25 +1,22 @@
 package kr.ac.kpu.s2015182034.samplegame;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Choreographer;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
-import java.util.logging.Handler;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class GameView extends View {
     // final 변수는 생성자에서 그 값이 결정되어야 한다.
     private static final String TAG = GameView.class.getSimpleName();
+    private static final int BALL_COUNT = 100;
 
-    private Ball b1;
-    private Ball b2;
+    ArrayList<Ball> balls = new ArrayList<>();
 
     // public : 외부에서 모르고도 접근할 수 있도록
     // static: 외부에서 해당 view 객체를 모르고도 접근할 수 있도록
@@ -45,9 +42,9 @@ public class GameView extends View {
 
     private void doGameFrame() {
         // update();   // 계산
-        b1.update();
-        b2.update();
-
+        for(Ball ball : balls) {
+            ball.update();
+        }
         // draw();     // 그리기
         // invalidate()함수는 여러번 중첩 호출되어도 한번에 그림으로써 중첩 호출을 해결
         invalidate();
@@ -67,14 +64,27 @@ public class GameView extends View {
                 });
     }
     private void InitResources() {
-        b1 = new Ball(100, 100, 100, 200);
-        b2 = new Ball(850, 100, -50, 150);
+        Random rand = new Random();
+        for(int i = 0; i < BALL_COUNT; ++i){
+            //float x = rand.nextInt(getWidth());
+            //float y = rand.nextInt(getHeight());
+            float x = rand.nextInt(1000);
+            float y = rand.nextInt(1000);
+            float dx = rand.nextFloat() * 1000 - 500;
+            float dy = rand.nextFloat() * 1000 - 500;
+            Ball ball = new Ball(x,y, dx, dy);
+            balls.add(ball);
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        b1.draw(canvas);
-        b2.draw(canvas);
+        //for(int i = 0; i < BALL_COUNT; ++i){
+        //    balls.get(i).draw((canvas));
+        //}
+        for(Ball b : balls){
+            b.draw(canvas);
+        }
         //Log.d(TAG,  "Drawing at : " + x + " y : " + y + " ft = " + frameTime);
     }
 }
