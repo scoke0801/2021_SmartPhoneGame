143bb5de50494333e4bd28436e61b758e0b58483
Lim Jong Hyun <scoke0801@gmail.com>
2021-04-16 11:02:01 +0900

    load함수를 상위 클래스인 GameBitmap 클래스로 이동

--- SampleGame/app/src/main/java/kr/ac/kpu/s2015182034/samplegame/framework/AnimationGameBitmap.java
+++ SampleGame/app/src/main/java/kr/ac/kpu/s2015182034/samplegame/framework/AnimationGameBitmap.java
@@ -4,13 +4,11 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 
 import java.util.HashMap;
 
-import kr.ac.kpu.s2015182034.samplegame.R;
 import kr.ac.kpu.s2015182034.samplegame.ui.view.GameView;
 
 public class AnimationGameBitmap extends GameBitmap {
@@ -25,19 +23,8 @@ public class AnimationGameBitmap extends GameBitmap {
 
     private int frameIndex;
 
-    private HashMap<Integer, Bitmap> bitmaps = new HashMap<Integer, Bitmap>();
-
     public AnimationGameBitmap(int resId, float framePerSecond, int frameCount){
-        bitmap = bitmaps.get(resId);
-        if (bitmap == null) {
-            Resources res = GameView.view.getResources();
-
-            BitmapFactory.Options opts = new BitmapFactory.Options();
-            opts.inScaled = false;
-
-            bitmap = BitmapFactory.decodeResource(res, resId, opts);
-            bitmaps.put(resId, bitmap);
-        }
+        bitmap = GameBitmap.load(resId);
 
         imageWidth = bitmap.getWidth();
         imageHeight = bitmap.getHeight();
