52544f3e13e3c0e3aa7c523ffffe17ba88201857
Lim Jong Hyun <scoke0801@gmail.com>
2021-04-23 11:21:15 +0900

    enemy level, wave 변경

--- DragonFlight/app/src/main/java/kr/ac/kpu/s2015182034/dragonflight/game/Enemy.java
+++ DragonFlight/app/src/main/java/kr/ac/kpu/s2015182034/dragonflight/game/Enemy.java
@@ -11,16 +11,24 @@ import kr.ac.kpu.s2015182034.dragonflight.framework.GameObject;
 
 public class Enemy implements GameObject, BoxCollidable {
     private static final float FRMAES_PRE_SECOND = 8.0f;
+    private static final int[] RESOURCE_IDS = {
+            R.mipmap.enemy_01, R.mipmap.enemy_02, R.mipmap.enemy_03, R.mipmap.enemy_04, R.mipmap.enemy_05,
+            R.mipmap.enemy_06, R.mipmap.enemy_07, R.mipmap.enemy_08, R.mipmap.enemy_09, R.mipmap.enemy_10,
+            R.mipmap.enemy_11, R.mipmap.enemy_12, R.mipmap.enemy_13, R.mipmap.enemy_14, R.mipmap.enemy_15,
+            R.mipmap.enemy_16, R.mipmap.enemy_17, R.mipmap.enemy_18, R.mipmap.enemy_19, R.mipmap.enemy_20,
+    };
     private final int speed;
     private final AnimationGameBitmap bitmap;
     private float x;
     private float y;
-
-    public Enemy(float x, float y, int speed){
+    private int level;
+    public Enemy(int level, float x, float y, int speed){
         this.x = x;
         this.y = y;
         this.speed = speed;
-        this.bitmap = new AnimationGameBitmap(R.mipmap.enemy_01,
+        this.level = level;
+        int resId =  RESOURCE_IDS[level - 1];
+        this.bitmap = new AnimationGameBitmap(resId,
                 FRMAES_PRE_SECOND, 0);
     }
     @Override
--- DragonFlight/app/src/main/java/kr/ac/kpu/s2015182034/dragonflight/game/EnemyGenerator.java
+++ DragonFlight/app/src/main/java/kr/ac/kpu/s2015182034/dragonflight/game/EnemyGenerator.java
@@ -3,18 +3,22 @@ package kr.ac.kpu.s2015182034.dragonflight.game;
 import android.graphics.Canvas;
 import android.util.Log;
 
+import java.util.Random;
+
 import kr.ac.kpu.s2015182034.dragonflight.UI.View.GameView;
 import kr.ac.kpu.s2015182034.dragonflight.framework.GameObject;
 
 public class EnemyGenerator implements GameObject {
     private static final String TAG = EnemyGenerator.class.getSimpleName();
-    private static float INITIAL_SPAWN_INTERVAL = 5.0f;
+    private static float INITIAL_SPAWN_INTERVAL = 2.0f;
     private float time;
     private float spawnInterval;
+    private int wave;
 
     public EnemyGenerator(){
         time =  0.0f;
         spawnInterval = INITIAL_SPAWN_INTERVAL;
+        wave = 0;
     }
     @Override
     public void update() {
@@ -26,11 +30,19 @@ public class EnemyGenerator implements GameObject {
     }
 
     private void generate() {
+        ++wave;
         Log.d(TAG, "EnemyGenerator::generate()");
         MainGame game = MainGame.get();
-        float xRate = GameView.view.getWidth() / 10;
-        for(int i = 1; i < 10; i += 2){
-            game.add(new Enemy(xRate *i, 0, 700));
+        int tenth = GameView.view.getWidth() / 10;
+        Random r = new Random();
+        for (int i = 1; i <= 9; i += 2) {
+            int x = tenth * i;
+            int y = 0;
+            int level = wave / 10 - r.nextInt(3);
+            if(level < 1) level = 1;
+            if(level > 20) level = 20;
+            Enemy enemy = new Enemy(level, x, y, 700);
+            game.add(enemy);
         }
     }
 
