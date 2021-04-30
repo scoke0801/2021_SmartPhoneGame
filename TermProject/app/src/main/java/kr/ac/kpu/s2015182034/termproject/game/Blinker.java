a4311bd69442b8f90b04607faaab987f10563263
Lim Jong Hyun <scoke0801@gmail.com>
2021-04-30 12:52:34 +0900

    아이템 하위 클래스 추가

---
+++ TermProject/app/src/main/java/kr/ac/kpu/s2015182034/termproject/game/Blinker.java
@@ -0,0 +1,42 @@
+package kr.ac.kpu.s2015182034.termproject.game;
+
+import android.graphics.Canvas;
+
+import kr.ac.kpu.s2015182034.termproject.framework.MainGame;
+import kr.ac.kpu.s2015182034.termproject.ui.view.GameView;
+
+public class Blinker extends Item {
+    Blinker(String type, float x, float y, boolean isLeft){
+        super();
+    }
+
+    @Override
+    public void update() {
+        if (isOnMove){
+        }
+        else{
+        }
+        float dx = MainGame.get().frameTime * this.speed;
+        if(isLeft){
+            dx = -dx;
+        }
+        x += dx;
+
+        int w = GameView.view.getWidth();
+        if(isLeft){
+            if( x < 0) {
+                this.x = w;
+            }
+        }
+        else{
+            if(x > w){
+                this.x = 0;
+            }
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        bitmap.draw(canvas, x, y);
+        bitmap.drawAABB(canvas, x, y);
+    }
+}
